0c6e2051113f185261c0f86539bb78d8e179cf76
WesGatner <wesgtr@gmail.com>
2022-11-09 18:11:07 -0300

    new git repo

---
+++ src/js/cart.js
@@ -0,0 +1,36 @@
+function getLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+
+function getCartContents() {
+  let markup = '';
+  const cartItems = getLocalStorage('so-cart');
+  if (cartItems) {
+    const htmlItems = cartItems.map((item) => renderCartItem(item));
+    document.querySelector('.product-list').innerHTML = htmlItems.join('');
+  } else {
+    document.querySelector('.product-list').innerHTML = '<li>Cart is empty!</li>';
+  }
+  // document.querySelector(".product-list").innerHTML = renderCartItem(cartItems);
+}
+
+function renderCartItem(item) {
+  const newItem = `<li class="cart-card divider">
+  <a href="#" class="cart-card__image">
+    <img
+      src="${item.Image}"
+      alt="${item.Name}"
+    />
+  </a>
+  <a href="#">
+    <h2 class="card__name">${item.Name}</h2>
+  </a>
+  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__price">$${item.FinalPrice}</p>
+</li>`;
+  console.log(newItem);
+  return newItem;
+}
+
+getCartContents();
